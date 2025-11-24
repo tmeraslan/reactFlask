4b90219d1d2eebc610bd63446700298545f75784
tmeraslan <tmeraslan1@gmail.com>
2025-11-24 23:00:23 +0200

    dockercompose

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ import styles from "./styles";
 // VITE_API_BASE=http://localhost:5000
 // const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 
-// const API_BASE = import.meta.env.VITE_API_BASE || window.location.origin;
-const API_BASE = "http://localhost:8080"
+const API_BASE = import.meta.env.VITE_API_BASE || window.location.origin;
+// const API_BASE = "http://localhost:8080"
 
 
 const CURRENCIES = ["USD", "EUR", "GBP", "JPY"];
